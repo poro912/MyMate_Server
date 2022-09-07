688147d96f9ccfea1861397e6ae691029bcccac5
DESKTOP-IC8IM2M\편성윤 <psy7217@naver.com>
2022-09-07 17:25:24 +0900

    project create

---
+++ MyMate_Server/MyMate_Server/Program.cs
@@ -0,0 +1,7 @@
+﻿using MyMate_Server.ServerModule;
+
+SQL sql = new();
+
+Console.WriteLine(sql.SignIn("mod", "1234", "rara", "roro", "010-3333-4444"));
+
+Console.WriteLine(sql.Login("test", "1234"));
