eac8ca04ae236fce0400820d2ca919da21f9058d
DESKTOP-IC8IM2M\편성윤 <psy7217@naver.com>
2022-09-07 18:08:45 +0900

    submodule test

--- MyMate_Server/MyMate_Server/Program.cs
+++ MyMate_Server/MyMate_Server/Program.cs
@@ -1,7 +1,18 @@
 ﻿using MyMate_Server.ServerModule;
 
+using MyMate_Network;
+
+Server server = Server.Instance;
+
+
 SQL sql = new();
 
 Console.WriteLine(sql.SignIn("mod", "1234", "rara", "roro", "010-3333-4444"));
 
 Console.WriteLine(sql.Login("test", "1234"));
+
+do
+{
+    //Server.Start();
+}
+while (true);
