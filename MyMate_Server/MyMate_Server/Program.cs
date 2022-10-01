deedbdc12db43b3df3b399deb0c8305cb05bc42a
DESKTOP-IC8IM2M\편성윤 <psy7217@naver.com>
2022-10-01 10:04:47 +0900

    update network

--- MyMate_Server/MyMate_Server/Program.cs
+++ MyMate_Server/MyMate_Server/Program.cs
@@ -1,19 +1,19 @@
 ﻿using MyMate_Server.ServerModule;
 
-using MyMate_Network;
+using ServerNetwork.Module;
 
 Server server = Server.Instance;
 
 SQL sql = new();
 
-//Console.WriteLine(sql.Check("sky","1234","haneul","sky","010-5566-7788",sql.CallSigninSP));
+//Console.WriteLine(sql.Check("sky","1234","haneul","sky","010-5566-7788",sql.Signin));
 
-//Console.WriteLine(sql.noResultConnectDB("sky","1234",sql.CallLoginSF));
+//Console.WriteLine(sql.noResultConnectDB("sky","1234",sql.Login);
 
 Console.WriteLine(sql.resultConnectDB("sky",sql.GetUserinfo));
 
 do
 {
-    // Server.Start();
+    // 스레드로 처리
 }
 while (true);
